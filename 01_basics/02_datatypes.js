457f355e772f1433acc9d2d9b2c244c4ab2a5ec6
Manojpatel-coder <124519634+Manojpatel-coder@users.noreply.github.com>
2025-01-13 20:00:00 +0000

    commit datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";
+//syntax defined to say intrepetor to use all newever version of js on file
+
+//ecma script defined how all component of javascipt should be defined
+
+//types of datatypes in javacript
+//number
+//bigInt
+//boolean
+//null standalone value
+//undefined value is not assign till now
+//symbol used when we want uniqueness --used mostly in react
+//object
+
+
+//type of null is object 
