25a6c78c0ab3ded51f6c725b80ce9a0c36b5da3a
Manojpatel-coder <124519634+Manojpatel-coder@users.noreply.github.com>
2025-01-13 20:00:37 +0000

    delete test

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+//here we are learning defining of varibale in javascipt
+//const var and let are keyword used to define variable
+//varible is container that store different type of data
+
+
+//in javascipt we can define data without using any varible keyword 
+school="gvs";//also works but not to use
+
+const name="manoj";//varible defined with const cannot be changed in future give error if you try to do so
+let rollNo=123;
+var college="Lnct";
+
+//prefer not to use var because it has function scope
+console.table([name,rollNo,college]);
+//method to show varible data in table form
