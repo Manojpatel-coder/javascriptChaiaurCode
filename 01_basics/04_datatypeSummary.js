7ece28f389bb6c9c1a381ed5ea26504b4e121e15
Manojpatel-coder <124519634+Manojpatel-coder@users.noreply.github.com>
2025-01-14 05:46:52 +0000

    04

--- 01_basics/04_datatypeSummary.js
+++ 01_basics/04_datatypeSummary.js
@@ -15,3 +15,18 @@ const id=Symbol('123');
 //uniquely identify itself 
 const another=Symbol('123');
 console.log(id===anotherId);//false
+
+
+
+
+//type of memory 
+//stack(primitive)  heap(non primitive)
+
+
+//non primitive data stored in heap and when we want data its orignial refernce is given
+ 
+
+
+//dates in javascript
+let date=new Date();
+console.log(date);
