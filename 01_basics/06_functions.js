7757c0427cd510ca12dc4520b46c786ca0380c6c
Manojpatel-coder <124519634+Manojpatel-coder@users.noreply.github.com>
2025-01-14 18:43:59 +0000

    fun

--- 01_basics/06_functions.js
+++ 01_basics/06_functions.js
@@ -11,3 +11,26 @@ function bundleToArray(...nums){
     return nums;//bundle all value to array
 }
 console.log(bundleToArray(3,4,2,4));
+
+
+
+//variable define with var keyword is done with hoist means all defining of var variable go to top of code if it is not declared then it is undefined otherwise if it is defined inside any block then also its value is giving to it;
+if(true){
+    var a=10;
+    let b=12;
+}
+console.log(a)//10
+console.log(b);//gives error
+
+
+
+//as we know two type of function declaration
+//if you call addOne before declartion it will exexute but addTwo which is declared as function expression type wil give error
+addOne();
+function addOne(num){
+    return num+1;
+}
+addTwo();//give error
+const addTwo=function(num){
+    return num+2;
+}
