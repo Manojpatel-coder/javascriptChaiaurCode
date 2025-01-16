88f3e061eb48e6bdc35a8c798110a94c7d983adf
Manojpatel-coder <124519634+Manojpatel-coder@users.noreply.github.com>
2025-01-16 02:32:31 +0000

    html and dom

---
+++ 01_basics/07_thisAndArrowFunction.js
@@ -0,0 +1,50 @@
+//this will print current object which we are dealing with 
+//console.log(this);
+//it will give empty object in node engine
+//but will give window objct when run on browser
+
+//arrow function and normal function return same empty object in node
+//arrow function in javascript has implicit return without using return vallue will be returned automatically
+const app=(num1,nums2)=>num1+nums2;//valid
+const app1=(num2,num3)=>(num1+num2);//valid
+//const app3=(num3,num4)=>{num1+num2};//invalid -----when you use curly braces then you have to write return statement
+
+//immemdiately invoking function
+//()()
+(function app(){
+    console.log("IIFE function ")
+})();//named IIFE
+//to pass parameters to it
+((num1)=>{
+    console.log(num1)
+})(3);//unnamed IIFE
+
+
+//dealing of forof loop on map 
+const map=new Map();
+map.set("a","1");
+map.set("b",2);
+map.set("c","why");
+for(const key of map){
+    console.log(key);
+}
+//by destructing value 
+for(const [key,value] of map){
+    console.log(`${key} and ${value}`);
+}//but does not work forof loop here in object
+
+
+//for iteration on object for in loop will be worked
+
+//foreach loop is used for iteration of array 
+const arr=['manoj','why','are','you'];
+arr.forEach((element)=>{
+    console.log(element);
+});//only work to run but to return map and filter is used
+
+
+//filter function on array will filter element based on condition save to new Array
+
+//reduce  method reduce a array to single value
+//array.reduce(callback,intialVAlue);
+//array.reduce((acc,ele)=>acc+el),0);
---
+++ 02_dom/dom.js
@@ -0,0 +1,8 @@
+//innerText and textContent are display the text inside element with text content also display hidden text
+
+//document.querySelector('input[type="password"]);
+
+//nodeList is returned when we apply qyerySelectorAll on h1
+//to change property 
+const myNodeList=document.querySelectorAll('h1');
+//to iterate or change property of nodeList nodeList[0],[1]...will be used
