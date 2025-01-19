a034f1dd628f0a05abc74a44e962bcd99eb1f6bb
Manojpatel-coder <124519634+Manojpatel-coder@users.noreply.github.com>
2025-01-19 06:11:24 +0000

    committhechanges

--- 02_dom/dom.js
+++ 02_dom/dom.js
@@ -6,3 +6,19 @@
 //to change property 
 const myNodeList=document.querySelectorAll('h1');
 //to iterate or change property of nodeList nodeList[0],[1]...will be used
+
+//nodelist has not property map
+//htmlcollection has not property forEach and map
+//first conevet it into array by Array.from(htmlcollection || nodeList);
+
+
+//parent.children give html collection to traverse
+//by children we can go to parent by parentElement
+
+
+//parent.childNodes give nodelist (collection with text div line break...)
+
+
+
+//we are creating a function to create element 
+
