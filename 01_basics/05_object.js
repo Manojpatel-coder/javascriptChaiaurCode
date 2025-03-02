b9288f362417c9bfa3326d93583f012e5726f438
Manojpatel-coder <124519634+Manojpatel-coder@users.noreply.github.com>
2025-03-03 03:37:12 +0530

    Update 05_object.js

--- 01_basics/05_object.js
+++ 01_basics/05_object.js
@@ -1,4 +1,10 @@
+//array has two type of copy shallow where same reference is given and deep copy wheere different referce
 //creation of singleton objects
+//arr.unshift(8);add at first
+//arr.includes arr.indexOf(val) give index if exist otherwise gives -1
+//slice and splice difference splice change the orignal array when returing also include last index arr.splice(1,3) also incldes three
+//const all_hero=[...arr1,...arr2];\
+//Array.from("MANOJ");
 const obj=new Object();
 
 //destructuring of objects
