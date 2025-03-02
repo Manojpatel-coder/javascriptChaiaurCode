67bf3908d9519d948d6c02b8402cf099d68d3a9c
Manojpatel-coder <124519634+Manojpatel-coder@users.noreply.github.com>
2025-03-03 04:25:21 +0530

    Update 05_object.js

--- 01_basics/05_object.js
+++ 01_basics/05_object.js
@@ -9,17 +9,23 @@ const obj=new Object();
 
 //destructuring of objects
 //getting a value from a object
+const mySym=new Symbol("key1");
 const obj1={
+    [mySym]:"myKey",//syntax to use symbol to retrivre obj1[mySym]
     name:"chaheti",
     age:1,
     address:"bhopal"
 }
+Object.freeze(obj1);//you cannot change object value know
 let {name}=obj1;
 console.log(name);
 //if you want to use differnt name in place of name then
 let {address:pta}=obj1;
 console.log(pta);
 
+//const obj3=Object.assign({},obj1,obj2);//copy value of obj1 and obj2 and place in paramater passed first(target)
+//Object.keys(obj1);return keys||values of obj1 in array
+//checking is property exist obj1.hasOwnProperty(property_name)//boolean
 
 //Json study in object
 //we can get api respone either as jpon(object) or array
