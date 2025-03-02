59b15654874f07a4509e1a5800f729489be3d513
Manojpatel-coder <124519634+Manojpatel-coder@users.noreply.github.com>
2025-03-03 01:24:33 +0530

    Update 01_variable.js

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,3 +1,8 @@
+//js was execute with browser only before but now the software that execute js file is now execute with node and similiar type software  
+//deno and node are software are js run time enviroment
+
+//****make new repo--there is code to create codeforce there where vs code type go command palaete and setup node there
+
 //here we are learning defining of varibale in javascipt
 //const var and let are keyword used to define variable
 //varible is container that store different type of data
@@ -10,6 +15,6 @@ const name="manoj";//varible defined with const cannot be changed in future give
 let rollNo=123;
 var college="Lnct";
 
-//prefer not to use var because it has function scope
+//prefer not to use var because it has function scope and block scope
 console.table([name,rollNo,college]);
 //method to show varible data in table form
