7d49eeadff34b541d0a987751da930169577ba94
Manojpatel-coder <124519634+Manojpatel-coder@users.noreply.github.com>
2025-03-07 03:46:22 +0530

    Update events.js

--- 03_events/events.js
+++ 03_events/events.js
@@ -1,11 +1,13 @@
 //piece of code occur when user perform some action
-//onclick gives less features then eventLisntener ,event listener have varity of events
+//onclick gives less features then eventLisntener ,event listener have varity of events 
+//onclick has not propagation thats why we use eventlistener
 //obj.addEventListener(eventName,callback,true/false)//by defalut it is false
-//it is event object which has many properties about the event like timestamp,event location ,target,tagName
+//** read about the event target timestamp clientX clientY
+//it has event object which has many properties about the event like timestamp,event location ,target,tagName
 //event lisetener by default behaviour is false which is works as eventbubbling means insideout (first inside event first then outside)
 //if you change it to true then it will works as eventcapturing means outside to inside
 
-//to stop these propagation stoppropagation() will be set
+//to stop these propagation stoppropagation() will be set stop bubble to outer element
 
 //to change the default behaviour of something the we use preventDefault()
 //event.target selects the elemnt by whom event occur
