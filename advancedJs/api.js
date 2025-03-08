b8497158cfd694bbe5a0cc6cc822b99b02496d05
Manojpatel-coder <124519634+Manojpatel-coder@users.noreply.github.com>
2025-03-08 12:10:22 +0530

    Update api.js

--- advancedJs/api.js
+++ advancedJs/api.js
@@ -3,3 +3,7 @@
 //before fetch method xmlhttprequest is used to get info from api
 //string data to json
 //const data=JSON.parse(string_data);
+//js is run through v8 engine which is written in c++
+
+//promise in javaScript
+
