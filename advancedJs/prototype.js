782057ea1c954b264ce71bf6c6351452619f5b74
Manojpatel-coder <124519634+Manojpatel-coder@users.noreply.github.com>
2025-03-08 21:43:28 +0530

    Update prototype.js

--- advancedJs/prototype.js
+++ advancedJs/prototype.js
@@ -61,11 +61,40 @@ const chai = new createUser("chai", "chai@fb.com", "123")
 console.log(chai);
 
 
-
+//the mention work of pasing execution can be done be super keyword also where inheritance comes in 
 //class constructor
+class User {
+    constructor(username){
+        this.username = username
+    }
 
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const hitesh = new User("hitesh")
+// console.log(hitesh.createId())
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId());
+ //the method defined with static keyword are not accesed by all its instances
+
+
+//bind in javascript learn it 
+//closue
  
-
 
 
 
